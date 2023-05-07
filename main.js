81c4f476bf9c5486d792b4b74994ad873b27e441
ChristiannHoang <hnhan793@gmail.com>
2023-05-07 17:36:37 +0700

    lam tiep topic2

--- main.js
+++ main.js
@@ -31,4 +31,39 @@ resultBtn.addEventListener('click', function (e) {
     }
 });
 
+// TOPIC 2
+const showOutput = document.querySelector('#topic-2__result');
+const topic2Btn = document.querySelector('#topic-2__btn');
+const numList = document.querySelectorAll('.topic-2 .form-control')
+const resetForm2 = document.querySelector('#topic-2__resetBtn');
+
+function Average(arr) {
+    var sum = 0;
+    for (var i = 0; i < arr.length; i++) {
+        sum += parseFloat(arr[i].value);
+    }
+    return (sum / 5).toFixed(4);
+}
+
+resetForm2.addEventListener('click', function (e) {
+    e.preventDefault();
+    for (i = 0; i < numList.length; i++) {
+        numList[i].value = '';
+        showOutput.innerHTML = '';
+    }
+});
+
+topic2Btn.addEventListener('click', function (e) {
+    e.preventDefault();
+    showOutput.innerHTML = Average(numList);
+    var issNaN = false;
+    for (var i = 0; i < numList.length; i++) {
+        if (isNaN(numList[i].value)) {
+            issNaN = true;
+            console.log(issNaN);
+        }
+    }
+})
+
+
 
